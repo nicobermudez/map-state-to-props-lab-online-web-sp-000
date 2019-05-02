bb1f739a482a9cd316dc5f3d2a7c28331419a985
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-02 22:01:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
-import manageUsers from './reducers/manageUsers'
 import { Provider } from 'react-redux'
 import { createStore } from 'react-redux'
+import App from './App'
+import manageUsers from './reducers/manageUsers'
+
 
 
 
