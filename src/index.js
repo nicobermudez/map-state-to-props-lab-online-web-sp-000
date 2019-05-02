b70b8c0874d5343c2184de44e1869eaffcbd33da
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-02 22:03:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import manageUsers from './reducers/manageUsers'
 const store = createStore(
   manageUsers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+);
 
 
 ReactDOM.render(
